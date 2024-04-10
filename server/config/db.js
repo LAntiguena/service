905dd204473146661c1b759241c823f731b5a4d4
Rapiwash <sistemarapiwash@gmail.com>
2024-04-09 20:01:47 -0500

    add project

---
+++ server/config/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://Antiguena:<password>@sistemalavanderia.5ra9cy7.mongodb.net/db-laundry-system?retryWrites=true&w=majority&appName=SistemaLavanderia",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log("Conexión exitosa a MongoDB");
+  } catch (error) {
+    console.error("Error al conectar a MongoDB:", error);
+    process.exit(1); // Terminar el proceso con error
+  }
+};
+
+export default mongoose;
